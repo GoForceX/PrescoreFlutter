a9cc5299912914effb4992cc91b0ef01ebc75e8e
GoForceX <goforcex@outlook.com>
2022-07-05 20:12:35 +0800

    fix: Use getApplicationSupportDirectory instead of TemporaryDirectory

--- lib/main.dart
+++ lib/main.dart
@@ -238,7 +238,7 @@ class BaseSingleton {
     dio.options.responseType = ResponseType.plain;
     dio.options.headers["User-Agent"] =
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.124 Safari/537.36 Edg/102.0.1245.41";
-    getTemporaryDirectory().then((value) {
+    getApplicationSupportDirectory().then((value) {
       String dataPath = value.path;
       cookieJar = PersistCookieJar(
           storage: FileStorage(
