6d37088a790c47b312dc9c8d9844214704d470ec
GoForceX <goforcex@outlook.com>
2022-08-28 14:45:21 +0800

    feat: Save paper photo to gallery

--- lib/widget/paper/paper_photo.dart
+++ lib/widget/paper/paper_photo.dart
@@ -1,7 +1,14 @@
+import 'dart:typed_data';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:prescore_flutter/main.dart';
 import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
+import 'dart:io';
 
 import '../../model/paper_model.dart';
 
@@ -63,44 +70,125 @@ class _PaperPhotoState extends State<PaperPhoto> {
       );
     }
 
-    return Center(
-        child: Column(children: [Expanded(child: main)]));
+    return Center(child: Column(children: [Expanded(child: main)]));
   }
 }
 
-class PaperPhotoWidget extends StatelessWidget {
+class PaperPhotoWidget extends StatefulWidget {
   final String url;
   final String tag;
   const PaperPhotoWidget({Key? key, required this.url, required this.tag})
       : super(key: key);
 
   @override
+  State<PaperPhotoWidget> createState() => _PaperPhotoWidgetState();
+}
+
+class _PaperPhotoWidgetState extends State<PaperPhotoWidget> {
+  @override
   Widget build(BuildContext context) {
+    GlobalKey globalKey = GlobalKey();
     return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: PhotoView(
-                      imageProvider: NetworkImage(
-                        url,
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (subContext) => GestureDetector(
+                      onTap: () {
+                        Navigator.pop(subContext);
+                      },
+                      child: Scaffold(
+                        floatingActionButton: FloatingActionButton(
+                          onPressed: () async {
+                            if (Platform.isAndroid) {
+                              PermissionStatus storageStatus =
+                                  await Permission.storage.status;
+                              if (!storageStatus.isGranted) {
+                                await Permission.storage.request();
+                              }
+                              if (storageStatus.isGranted) {
+                                Dio dio = BaseSingleton.singleton.dio;
+                                dio
+                                    .get(widget.url,
+                                        options: Options(
+                                            responseType: ResponseType.bytes))
+                                    .then((value) async {
+                                  final result = await ImageGallerySaver.saveImage(
+                                      Uint8List.fromList(value.data),
+                                      name:
+                                          "prescore_${(DateTime.now().millisecondsSinceEpoch / 100).round()}");
+                                  if (mounted) {
+                                    if (result["isSuccess"]) {
+                                      SnackBar snackBar = SnackBar(
+                                        content: const Text('保存大成功！'),
+                                        backgroundColor:
+                                            ThemeMode.system == ThemeMode.dark
+                                                ? Colors.grey[900]
+                                                : Colors.grey[200],
+                                      );
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(snackBar);
+                                    } else {
+                                      SnackBar snackBar = SnackBar(
+                                        content: Text(
+                                            '呜呜呜，失败了……\n失败原因：${result["errorMessage"]}'),
+                                        backgroundColor:
+                                            ThemeMode.system == ThemeMode.dark
+                                                ? Colors.grey[900]
+                                                : Colors.grey[200],
+                                      );
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(snackBar);
+                                    }
+                                  }
+                                });
+                              } else {
+                                SnackBar snackBar = SnackBar(
+                                  content: Text(
+                                      '呜呜呜，失败了……\n失败原因：无保存权限……'),
+                                  backgroundColor:
+                                  ThemeMode.system == ThemeMode.dark
+                                      ? Colors.grey[900]
+                                      : Colors.grey[200],
+                                );
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(snackBar);
+                              }
+                            } else {
+                              SnackBar snackBar = SnackBar(
+                                content:
+                                    const Text('呜呜呜，失败了……\n还不支持其他系统保存图片哦……'),
+                                backgroundColor:
+                                    ThemeMode.system == ThemeMode.dark
+                                        ? Colors.grey[900]
+                                        : Colors.grey[200],
+                              );
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                            }
+                          },
+                          child: const Icon(Icons.save_alt_rounded),
+                        ),
+                        body: PhotoView(
+                          imageProvider: NetworkImage(
+                            widget.url,
+                          ),
+                          heroAttributes:
+                              PhotoViewHeroAttributes(tag: widget.tag),
+                        ),
                       ),
-                      heroAttributes: PhotoViewHeroAttributes(tag: tag),
-                    ),
-                  )),
-        );
-      },
-      child: Hero(
-        tag: tag,
-        child: Image.network(
-          url,
-          width: 350.0,
-        ),
-      ),
-    );
+                    )),
+          );
+        },
+        child: Hero(
+          tag: widget.tag,
+          child: RepaintBoundary(
+            key: globalKey,
+            child: Image.network(
+              widget.url,
+              width: 350.0,
+            ),
+          ),
+        ));
   }
 }
