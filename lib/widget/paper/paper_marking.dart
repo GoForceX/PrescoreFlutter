d398ef83bbe7d59722bc707ef039013f09cf41c7
GoForceX <goforcex@outlook.com>
2024-04-27 19:14:50 +0800

    fix(paper): Apply UI changes to paper marking

--- lib/widget/paper/paper_marking.dart
+++ lib/widget/paper/paper_marking.dart
@@ -252,15 +252,8 @@ class QuestionProgressCardState extends State<QuestionProgressCard>
             ),
           ],
         ));
-    return Card(
-        elevation: 2,
+    return Card.filled(
         margin: const EdgeInsets.all(8.0),
-        shape: RoundedRectangleBorder(
-          side: BorderSide(
-            color: Theme.of(context).colorScheme.outlineVariant,
-          ),
-          borderRadius: const BorderRadius.all(Radius.circular(12)),
-        ),
         child: body);
   }
 }
